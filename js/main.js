a5821dbe6355885660aa90de27450d842247b175
harikays <58000936+harikays@users.noreply.github.com>
2021-08-24 11:18:07 +0800

    finallab
    NUR SYAKIRAH MOHAMED SHAH BERI

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*function myFunction() {
+  var x = document.getElementById("header-top");
+  if (x.className === "header-menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "header-menu";
+  }
+}*/
+
+
+//var bars = document.querySelector(".bars");
+//var headerbar = document.querySelector(".headerbar");
+
+//bars.addEventListener("click", function(){
+//  headerbar.classList.toggle("active");
+//})
+
